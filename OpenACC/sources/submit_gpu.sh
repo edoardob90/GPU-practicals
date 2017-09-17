df89e288c243b9ba8b0dd7fc504d8b426b3a5b79
Edoardo Baldi <edoardob90@gmail.com>
2017-09-17 12:16:35 +0200

    GPU submission script

--- OpenACC/sources/submit_gpu.sh
+++ OpenACC/sources/submit_gpu.sh
@@ -16,7 +16,7 @@ elif [[ -f $EXE_TO_LAUNCH && -x $EXE_TO_LAUNCH ]]; then
 #SBATCH -o $(basename -s .x "$EXE_TO_LAUNCH")_gpu.out
 module load gcc cuda
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-srun "$EXE_TO_LAUNCH" 2>&1
+srun --hint=nomultithread "$EXE_TO_LAUNCH" 2>&1
 EOF
 else
     echo "$EXE_TO_LAUNCH not found or not a compiled exe!" >&2; exit -1
